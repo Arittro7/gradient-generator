fe141df7f6057beec66df3a8a7da3fc82e2a5db2
Arittro7 <arittrowpdev@gmail.com>
2026-04-18 01:43:54 +0600

    dynamic gradient tiles created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/purity */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [num, setNum] = useState(12);
   const [type, setType] = useState("linear");
+  const [gradient, setGradient] = useState([]);
 
   // generate hex code for color
   const getHexCode = () => {
@@ -16,6 +17,25 @@ const App = () => {
     const hexCode = hexColorCode.padStart(6, "0");
     return `#${hexCode}`;
   };
+
+  const gradientGenerate = ()=>{
+    const colors = []
+    for(let i=0; i<num; i++)
+    {
+      const color1 = getHexCode()
+      const color2 = getHexCode()
+      const degree = Math.floor(Math.random() * 360)
+      const degreeString = `${degree}deg`
+      colors.push({
+        gradient: `linear-gradient (${degreeString}, ${color1}, ${color2})`
+      })
+    }
+    setGradient(colors)
+  }
+
+  useEffect(()=>{
+    gradientGenerate()
+  }, [num])
 
   return (
     <div className="min-h-screen bg-[#EFD2B0]">
@@ -45,50 +65,20 @@ const App = () => {
         </div>
 
         <div className="grid grid-cols-4 gap-4">
-        {/* color tiles - 1 */}
-          <div
-            className="h-45 rounded-xl relative"
-            style={{
-              background: getHexCode(),
-            }}
-          >
-            <button className="absolute bg-black/50 hover:bg-black text-white p-1 rounded-xl right-2 bottom-3">
-              COPY
-            </button>
-          </div>
-        {/* color tiles - 2 */}
-          <div
-            className="h-45 rounded-xl relative"
-            style={{
-              background: getHexCode(),
-            }}
-          >
-            <button className="absolute bg-black/50 hover:bg-black text-white p-1 rounded-xl right-2 bottom-3">
-              COPY
-            </button>
-          </div>
-        {/* color tiles - 3 */}
-          <div
-            className="h-45 rounded-xl relative"
-            style={{
-              background: getHexCode(),
-            }}
-          >
-            <button className="absolute bg-black/50 hover:bg-black text-white p-1 rounded-xl right-2 bottom-3">
-              COPY
-            </button>
-          </div>
-        {/* color tiles - 4 */}
-          <div
-            className="h-45 rounded-xl relative"
-            style={{
-              background: getHexCode(),
-            }}
-          >
-            <button className="absolute bg-black/50 hover:bg-black text-white p-1 rounded-xl right-2 bottom-3">
-              COPY
-            </button>
-          </div>
+          {/* color tiles  */}
+          {gradient.map((item, index) => (
+            <div
+            key={index}
+              className="h-45 rounded-xl relative"
+              style={{
+                background: getHexCode(),
+              }}
+            >
+              <button className="absolute bg-black/50 hover:bg-black text-white p-2 rounded-xl right-2 bottom-3">
+                COPY
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
